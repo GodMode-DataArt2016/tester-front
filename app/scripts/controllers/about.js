6b3ee9ee26d9804da9e4abe6d7d49ff1c3dd3899
gnordbs <gnordbs@gmail.com>
2016-04-25 23:12:11 +0300

    yeoman generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testerFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testerFrontApp
+ */
+angular.module('testerFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testerFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testerFrontApp
+ */
+angular.module('testerFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
