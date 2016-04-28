ec03c440d7a449ae8cdf5098bc3564b73009276a
gnordbs <gnordbs@gmail.com>
2016-04-29 00:01:44 +0300

    submit button

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,13 +17,6 @@ angular.module('testerFrontApp')
 
 angular.module('testerFrontApp')
 	.controller('TestCtrl', ['$scope', '$routeParams', 'Test', 'Question', function($scope, $routeParams, Test, Question) {
-		/*$scope.test = Test.get({testId: $routeParams.testId}, function(test) {
-			$scope.questions = test.questions[0];
-		});*/
-		
-		
-		
-		
 		Test.get({ id: $routeParams.testId}, function(data) {
 			$scope.test = data;
 			
@@ -31,16 +24,75 @@ angular.module('testerFrontApp')
 			var questionIDs = data.questions;
 			
 			questionIDs.forEach(function (ID) {
-				Question.get({ id: ID}, function(data) {
-					questions.push(data);	
+				Question.get({ id: ID}, function(data) {									
+					var allAnswers = {};
+					data.allAnswers.forEach(function(item){
+						allAnswers[item] = false;	
+					});
+					
+					data.allAnswers = allAnswers;
+
+					if(data.type == 'radio'){
+						data.radioAnswer = null;	
+					}
+					if(data.type == 'text'){
+						data.textAnswer = null;	
+					}
+					questions.push(data);					
 				});
-			});
-			
+			});		
 			$scope.questions = questions;
 		});
 		
 		
+		$scope.submitArray = null;
 		
+		$scope.submitAnswer = function() {
+			var verified = true;
+			var submitArray = [];
+			
+			submitArray.userForm = $scope.userForm;
+			
+			$scope.questions.forEach(function(item){
+				var answerInfo = {};
+				answerInfo.id = item.id;
+				switch (item.type) {
+					case "radio":
+						answerInfo.answer = item.radioAnswer;
+					break;
+					case "text":
+						answerInfo.answer = item.textAnswer;
+					break;
+					case "checkbox":
+						var cbVerified = false;
+						for(var index in item.allAnswers){
+							if(item.allAnswers[index]){ 
+								var cbVerified = true
+							}
+						}
+						if(cbVerified){
+							answerInfo.answer = item.allAnswers;	
+						} else {
+							verified = false;	
+						}											
+					break;
+				}
+				if(answerInfo.answer){
+					submitArray.push(answerInfo);
+				} else {
+					verified = false;				
+				}
+				
+			});
+					
+			if(verified){
+				alert("success");	
+			} else {
+				alert("answer all the questions");		
+			}
+			console.log(submitArray);
+		};	
 }]);
+
 
 
